8024ee2f79214541fdc2d1f8f44a8544c9f507fb
Nivrita44 <107574615+Nivrita44@users.noreply.github.com>
2023-07-31 19:16:05 +0600

    start

---
+++ JavaGame/src/main/java/com/example/javagame/HelloApplication.java
@@ -0,0 +1,31 @@
+package com.example.javagame;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.util.Random;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+import javafx.util.Duration;
+
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
